c5cfaaa1c9611a1d00c97b2a676bbb1db4c1643d
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-06 16:13:05 -0700

    wrote test to test detail page

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ describe('backend-zodiac-api routes', () => {
     return setup(pool);
   });
 
+  it('/zodiacs/:id responds with a single zodiac', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: 1,
+      name: 'Aquarius',
+      dates: 'jan 21 - feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
+
   afterAll(() => {
     pool.end();
   });
